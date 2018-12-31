d10171ba001974ee36493d2d62d75e5e13a8da32
Bhavya <gobhavyaarora15@gmail.com>
2018-12-31 12:04:16 +0530

    Add BoundService Example

---
+++ BoundService/app/src/main/java/bhavya/me/boundservice/MainActivity.java
@@ -0,0 +1,168 @@
+package bhavya.me.boundservice;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import bhavya.me.boundservice.service.BoundService;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Source(Full Explaination) : https://www.truiton.com/2014/11/bound-service-example-android/
+    //Source(Theory) : http://codetheory.in/understanding-android-started-bound-services/
+    //Source(Theory) : https://developer.android.com/guide/components/services
+
+    /*In this Example i'm using Chronometer class for running the timer in the background
+    * with the help of service. Basically Services are of 3 Types:
+    *   1. Service(Intent Service/ Simple extending from Service class)
+    *   2. ForeGround Service(Head Notification will be used.)
+    *   3. Bound Service(Any component of the application can interact or request something
+    *   from the service. that means any component(Activity, BR) can bind to service and
+    *   control the service from the Activity itself)
+    *
+    * Services are either StartedService( Service/IntentService ) or BoundService but in case of Music Player, we can
+    * use both ForegroundService( for Notification that tell user that is something is happening
+    * behind the scene ) AND BoundService( for Controlling the Music like: getting music name
+    * it's like client - server architecure ).
+    *
+    * So I used both startedService and BoundService in this example, Because BoundService is
+    * bound to the component which is started that, like in this case if MainActivity(Component) is
+    * Minimize so onStop() will call and Activity( MainActivity ) is unbind() from the service
+    * and service will stop, so timer will stop.
+    *
+    * But I'm also using startedService with BoundService, in startedService on binding will occur
+    * and after running the startedService even the component( MainAcitivty ) which started the
+    * service will get minimized( onStop() ) even after that service will not stop and timer will
+    * continue to run even the app is minimized.
+    *
+    * @@Callbacks:
+    * >>StartedService -> startService() > onCreate() > onStartCommand() > [Service is Running] > onDestroy() > [Service is ShutDown]
+    *
+    * >>BoundService -> bindService() > onCreate() > onBind() > [Client is bind to service using IBinder object which is returned from the onbind() in the ServiceConnection ] > onUnbind() > onDestroy() > [Service is Shutdown]
+    *
+    * NOTE: 1. IntentService is the subclass of Service class because if we inherit or use Service
+    *       class then we have to implement Thread by ourself but with the help of IntentService, Thread
+    *       is automatically created when we extend from IntentService( in onHandleIntent() ).
+    *
+    *       2. In StartedService( Service/ IntentService ), if we want to stop the service, we have
+    *       two Options. First by calling stopService() method by passing Intent object and Second,
+    *       if Service itself will stop by stopSelf() method.
+    *
+    *       3. In BoundService, if we want to stop the boundService we have to unBind() all the
+    *       components which is connected or bind with the service so that service will automatically
+    *       will shutdown.
+    * */
+
+    BoundService mBoundService;
+    boolean mServiceBound = false;
+    
+    public static final String TAG = "Bhavya";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: ");
+        
+        final TextView timestampText = (TextView) findViewById(R.id.timestamp_text);
+        Button printTimestampButton = (Button) findViewById(R.id.print_timestamp);
+        Button stopServiceButon = (Button) findViewById(R.id.stop_service);
+
+        printTimestampButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.i(TAG, "onClick: printTimeStamp");
+                if(mServiceBound)
+                    timestampText.setText(mBoundService.getTimestamp());
+            }
+        });
+
+        stopServiceButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mServiceBound){
+                    unbindService(mServiceConnection);
+                    mServiceBound = false;
+                }
+                Intent intent = new Intent(MainActivity.this, BoundService.class);
+                stopService(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(MainActivity.this, BoundService.class);
+        //Started Service
+        startService(intent);
+        //Bound Service
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop: ");
+        
+        if (mServiceBound) {
+            unbindService(mServiceConnection);
+            mServiceBound = false;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) iBinder;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
+
+            Log.i(TAG, "onServiceConnected: ");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mServiceBound = false;
+        }
+    };
+}
---
+++ BoundService/app/src/main/java/bhavya/me/boundservice/service/BoundService.java
@@ -0,0 +1,80 @@
+package bhavya.me.boundservice.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Chronometer;
+
+import bhavya.me.boundservice.MainActivity;
+
+import static bhavya.me.boundservice.MainActivity.TAG;
+
+public class BoundService extends Service {
+
+    private IBinder mBinder = new MyBinder();
+    private Chronometer mChronometer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mChronometer = new Chronometer(this);
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        mChronometer.start();
+    }
+
+    //onStartCommand() method will call only when startService() method will call but in this case both onStartCommand() and onBind() methods will call.
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
+    //OnBind() method will call only when bindService() method will call
+    //NOTE: only one IBinder object will make, only when first time this method will call, after that if any activity wants to bind to this service this method won't call again and again, but instead this class will send same IBinder object to anyone which want to bind to this service.
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, "onBind: ");
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mChronometer.stop();
+    }
+
+    //This method will call only when BoundService will use because in simple startedService(Service/IntentService)
+    // no response from the service can sent back to the calling component( MainActivity ), but in
+    // BoundService we can call this method from the Component( MainActivity ).
+    public String getTimestamp() {
+
+        Log.i(TAG, "getTimestamp: ");
+        long elapsedMillis = SystemClock.elapsedRealtime()
+                - mChronometer.getBase();
+        int hours = (int) (elapsedMillis / 3600000);
+        int minutes = (int) (elapsedMillis - hours * 3600000) / 60000;
+        int seconds = (int) (elapsedMillis - hours * 3600000 - minutes * 60000) / 1000;
+        int millis = (int) (elapsedMillis - hours * 3600000 - minutes * 60000 - seconds * 1000);
+
+        Log.i(TAG, "getTimestamp: " + hours + ":" + minutes + ":" + seconds + ":" + millis);
+        return hours + ":" + minutes + ":" + seconds + ":" + millis;
+    }
+
+    public class MyBinder extends Binder{
+        public BoundService getService(){
+            return BoundService.this;
+        }
+    }
+}
